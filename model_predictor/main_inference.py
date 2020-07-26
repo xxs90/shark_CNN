7f24950c3ee7cc2c6ddfabe48af963b5f40b8000
Jeremy <47799977+JeremyFJ@users.noreply.github.com>
2020-07-26 12:15:30 -0400

    Add files via upload

---
+++ model_predictor/main_inference.py
@@ -0,0 +1,85 @@
+# MIT License
+
+# Copyright (c) 2019 De Leo Lab- Hopkins Marine Station, Stanford University
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+"""
+Main script for running the trained CNN model for validation dataset; no trainings performed.
+See our method paper:
+Liu ZY-C, Chamberlin AJ, Shome P, Jones IJ, Riveau G, Jouanard N, Ndione, Sokolow SH, 
+De Leo GA. Snail and Parasite Image Classification using Deep Convolutional 
+Neural Networks: Application to Human Schistosomiasis Environmental Risk Monitoring. 
+PLoS neglected tropical diseases. 2019 (submitted).
+Utility script:
+model_validation.py: load preprocessed data, CNN architecture, model building, plot confusion matrix
+plot_confusion_matrix.py: plot confusion matrix on the prediction results of test set
+After running the main script, the accuracy and confusion matrix will be printed,
+and the plots of training histroy and confusion matrix (results) will be generated. 
+For questions, email: zacqoo@gmail.com 
+"""
+
+# ------------------------------------------------------------------------------ 
+# import libraries and packages
+from keras import applications  
+from model_inference import model_validation
+import pandas as pd
+# ------------------------------------------------------------------------------ 
+# define parameters
+in_s = 256 # input image size 
+img_width, img_height = in_s, in_s  
+  
+top_model_weights_path = 'bottleneck_shark_model.h5'  
+train_data_dir = 'dataset/training_set'  
+validation_data_dir = 'dataset/test_set'  
+# number of epochs to train top model  
+epochs = 50
+# batch size used by flow_from_directory and predict_generator  
+batch_size = 128
+# ------------------------------------------------------------------------------  
+# create the VGG16 model - 
+# without the final fully-connected layers (by specifying include_top=False) 
+# - and load the ImageNet weights
+model = applications.VGG16(include_top=False, weights='imagenet') 
+# ------------------------------------------------------------------------------ 
+# training
+class_predicted, test_label, pred = model_validation(img_width, img_height, top_model_weights_path, train_data_dir, 
+												validation_data_dir, epochs, batch_size, model)
+
+
+df = pd.DataFrame(list(zip(test_label, class_predicted)), columns =['filename', 'class_predicted']) 
+df_pred = pd.DataFrame(data=pred, columns=['0', '1', '2'])
+df_pred.to_csv('prediction_probability.csv', index=False)
+
+# change treshold
+classes = df_pred > 0.63
+threshold_class = []
+for i in range(classes.shape[0]):
+    if classes['2'][i] == False:
+        threshold_class.append(0)
+    elif classes['2'][i] == True:
+        threshold_class.append(1)
+    else:
+    	continue
+del df['class_predicted']
+d = pd.DataFrame(threshold_class)
+df['class_predicted'] = d
+
+df.to_csv('class_prediction.csv', index=False)
+# df.to_csv('/mnt/d/Portfolio-JJ/Instagram_Pipeline/Intermediary/Predictions/class_prediction.csv', index=False)
